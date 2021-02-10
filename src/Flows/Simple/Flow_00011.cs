a0ef41f66236b2bd5021ebcec321a4904b832fec
Bedirhan Urgun <bedirhanurgun@gmail.com>
2021-02-10 12:08:09 +0300

    Using new source and sink definitions

--- src/Flows/Simple/Flow_00011.cs
+++ src/Flows/Simple/Flow_00011.cs
@@ -2,19 +2,15 @@
 {
     public class Flow_00011
     {
-        public const string ID = "00011";
-        public const string Description = "A return value taint propagation";
-        public const bool IsVulnerable = true;
-
         public void Run()
         {
-            string input = System.Console.ReadLine();
+            string input = FlowBlot.Model.Framework.Source();
             string colored = Color(input);
 
-            /*FLOW:Flow_00011 - A return value taint propagation:OS Command Injection:7:FIND_ISSUE:1:
+            /*FLOW:Flow_00011 - A return value taint propagation:codethreat.flowblot.benchmark:7:FIND_ISSUE:1:
              *STEP_PATH:ABC
              */
-            System.Diagnostics.Process.Start(colored);
+            FlowBlot.Model.Framework.Sink(colored);
         }
 
         public string Color(string input)
